e6068a731e96b624bd8a7b7353fa08004cb987b9
oal100484 <ana.lopes@open-co.com>
2023-01-16 20:30:31 -0300

    refactored slack api request and tests

--- src/slackapi.py
+++ src/slackapi.py
@@ -1,17 +1,30 @@
 import json
 import requests
+from typing import Type
 from settings import Settings
-from typing import Type, Dict
+from collections import namedtuple
+from requests import Request, Response
 
 
 class SlackAPI:
     def __init__(self) -> None:
         self.url = Settings.WEBHOOK_SLACK
         self.header = {"Content-type": "application/json"}
+        self.default_return = namedtuple("SlackAPI", "status_code request response")
 
-    def webhook(self, data: Dict) -> str:
+    def __send_http_request(self, req_prepared: Type[Request]) -> Type[Response]:
+        session = requests.Session()
+        return session.send(req_prepared)
+
+    def webhook(self, data: dict) -> any:
         channel = Settings.SLACK_CHANNEL_TEST
-        response = requests.post(
-            url=f"{self.url}/{channel}", data=json.dumps(data), headers=self.header
+        req = Request(
+            method="POST",
+            url=f"{self.url}/{channel}",
+            data=json.dumps(data),
+            headers=self.header,
         )
-        return response
+        response = self.__send_http_request(req_prepared=req.prepare())
+        return self.default_return(
+            status_code=response.status_code, request=req, response=response.text
+        )
--- tests/slackapi_test.py
+++ tests/slackapi_test.py
@@ -1,22 +1,39 @@
+import json
 from pytest import fixture
+from http import HTTPStatus
 from settings import Settings
 from src.slackapi import SlackAPI
 
 
 @fixture
 def url():
-    return Settings.WEBHOOK_SLACK
+    hooks_slack = Settings.WEBHOOK_SLACK
+    channel = Settings.SLACK_CHANNEL_TEST
+    return f"{hooks_slack}/{channel}"
 
 
 @fixture
-def channel():
-    return Settings.SLACK_CHANNEL_TEST
+def data():
+    return {"text": "Hello, World!"}
 
 
-def test_webhook_send_message_successfully(requests_mock, url, channel):
-    requests_mock.post(url=f"{url}/{channel}", status_code=200, text="ok")
+def test_webhook_send_message_successfully(requests_mock, url, data):
+    requests_mock.post(url=url, status_code=200, text="ok")
+    send_message = SlackAPI().webhook(data=data)
+    assert send_message.request.method == "POST"
+    assert send_message.request.url == url
+    assert send_message.request.data == json.dumps({"text": "Hello, World!"})
+    assert send_message.status_code == HTTPStatus.OK
+    assert send_message.response == "ok"
+    assert isinstance(send_message.response, str)
 
-    slack_api = SlackAPI()
-    response = slack_api.webhook(data={"text": "Hello, World!"})
-    assert response.text == "ok"
-    assert response.status_code == 200
+
+def test_webhook_send_message_error(requests_mock, url, data):
+    requests_mock.post(url=url, status_code=400, text="invalid_payload")
+    send_message = SlackAPI().webhook(data=data)
+    assert send_message.request.method == "POST"
+    assert send_message.request.url == url
+    assert send_message.request.data == json.dumps({"text": "Hello, World!"})
+    assert send_message.status_code == HTTPStatus.BAD_REQUEST
+    assert send_message.response == "invalid_payload"
+    assert isinstance(send_message.response, str)
